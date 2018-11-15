b45e576032cf94d8e43e8c474da4eb879ab8bde4
justin j. moses <justinjmoses@gmail.com>
2018-11-15 11:43:49 -0500

    Adding

--- __tests__/eosio.token.js
+++ __tests__/eosio.token.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const { sendTransaction } = require(`../utils`);
+// const { sendTransaction } = require('../utils');
 
 // const { CONTRACT_ACCOUNT } = process.env;
 
 describe(`contract`, () => {
-  beforeEach(async () => {
-    const result = await sendTransaction({ name: `testreset` });
-    console.dir(result);
-  });
+  // beforeEach(async () => {
+  //   const result = await sendTransaction({ name: `testreset` });
+  //   console.dir(result);
+  // });
 
   test(`something`, async () => {
     expect(true).toBe(true);
