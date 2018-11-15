910340fe352fd9d857776de64154bf1d10fad9ff
justin j. moses <justinjmoses@gmail.com>
2018-11-15 11:41:54 -0500

    Adding

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['last', 'plugin:import/errors'],
+  parserOptions: {
+    sourceType: 'script',
+  },
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': 'off',
+    semi: 'error',
+    strict: ['error', 'global'],
+    'import/no-unresolved': ['error', { commonjs: true }],
+    'prefer-arrow-callback': 'error',
+    'space-before-blocks': ['error', 'always'],
+    'prefer-const': 'error',
+    'no-shadow': 'error',
+  },
+};
---
+++ __tests__/eosio.token.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { sendTransaction } = require(`../utils`);
+
+// const { CONTRACT_ACCOUNT } = process.env;
+
+describe(`contract`, () => {
+  beforeEach(async () => {
+    const result = await sendTransaction({ name: `testreset` });
+    console.dir(result);
+  });
+
+  test(`something`, async () => {
+    expect(true).toBe(true);
+  });
+});
