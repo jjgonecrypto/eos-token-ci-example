0c501b9d89f55608858b9c13f8df241f9cdbbbaf
justin j. moses <justinjmoses@gmail.com>
2018-11-15 15:44:05 -0500

    Commentary

--- compile-contracts.sh
+++ compile-contracts.sh
@@ -4,4 +4,6 @@ set -eox pipefail
 
 mkdir -p build;
 pushd build;
+# running this outside of the same folder where the WASM is generated seems to not generate a correct ABI
+# https://github.com/EOSIO/eosio.cdt/issues/281
 eosio-cpp ../eosio.contracts/eosio.token/src/eosio.token.cpp -I ../eosio.contracts/eosio.token/include/ -o eosio.token.wasm --abigen || popd
