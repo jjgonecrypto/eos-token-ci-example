90e3ef0000a927ca2d309eb212cbd3b3ff609b6c
justin j. moses <justinjmoses@gmail.com>
2018-11-16 18:27:37 -0500

    Fixing

--- docker/start-nodeos.sh
+++ docker/start-nodeos.sh
@@ -9,23 +9,9 @@ exec nodeos \
   --producer-name=eosio \
   --plugin eosio::chain_api_plugin \
   --plugin eosio::history_api_plugin \
-  --verbose-http-errors
-
-# full node startup (for reference)
-# exec nodeos \
-#   --http-server-address=0.0.0.0:7777 \
-#   --plugin=eosio::producer_plugin \
-#   --plugin=eosio::http_plugin \
-#   --plugin=eosio::chain_api_plugin \
-#   --plugin=eosio::history_plugin \
-#   --plugin=eosio::history_api_plugin \
-#   --plugin=eosio::http_plugin \
-#   --data-dir=/mnt/dev/data \
-#   --enable-stale-production \
-#   --producer-name=eosio \
-#   --config-dir=/mnt/dev/config \
-#   --access-control-allow-origin=* \
-#   --contracts-console \
-#   --http-validate-host=false \
-#   --verbose-http-errors \
-#   --filter-on=*
+  --plugin=eosio::http_plugin \
+  --plugin=eosio::producer_plugin \
+  --verbose-http-errors \
+  --http-validate-host=false \
+  --max-transaction-time=10000 \
+  --filter-on=*
