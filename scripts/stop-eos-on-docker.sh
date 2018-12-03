5ca7baa3e676afc51cbbcb20d1574a9a11def1b8
justin j. moses <justinjmoses@gmail.com>
2018-12-03 16:44:31 -0500

    Migrating to eosio-ci for the docker scripts

--- scripts/stop-eos-on-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eox pipefail
-
-# this will also remove the containers as they were started with the --rm flag
-docker ps --last 2 --filter "ancestor=justinjmoses/eosio-ci" -q | xargs 'docker' 'stop' '-t=1'
